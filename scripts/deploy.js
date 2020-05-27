62d23a12e7b8527ab216802e62e912cf930af0b0
Aaron <aaronhperez@gmail.com>
2020-05-27 18:47:04 +0200

    Build 1.0.0 at 2020-05-27T16:47:04

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,11 +5,10 @@ const { version } = require('../package.json');
 // Change Working directory
 process.chdir('dist/spa');
 
-
-execSync(`git init`, { stdio: 'inherit' })
-execSync(`git add - A`, { stdio: 'inherit' })
+// execSync(`git remote add origin git@github.com:AaronHernandezPerez/covid-tracker-front-dist.git`, { stdio: 'inherit' })
+execSync(`git add -A`, { stdio: 'inherit' })
 
 const dateNow = new Date().toISOString().split('.').shift();
-execSync(`git commit -m 'Build ${version} at ${dateNow}`, { stdio: 'inherit' })
+execSync(`git commit -m "Build ${version} at ${dateNow}"`, { stdio: 'inherit' })
 
-execSync(`git push -f git@github.com:AaronHernandezPerez/covid-tracker-front-dist.git master`, { stdio: 'inherit' })
+execSync(`git push -f git@github.com:AaronHernandezPerez/covid-tracker-front-dist.git origin master`, { stdio: 'inherit' })
