70a3795fdee075b5cbdc0b21f43520f281449d59
Aaron <aaronhperez@gmail.com>
2020-05-27 19:18:41 +0200

    Added deploy script

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -9,5 +9,3 @@ git add -A
 git commit -m 'Build  %DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2% %TIME:~0,2%:%TIME:~3,2%' 
 
 git push -f git@github.com:AaronHernandezPerez/covid-tracker-front-dist.git master
-
-REM quasar build && 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,31 @@
 const process = require('process');
 const { execSync } = require('child_process');
 const { version } = require('../package.json');
+const fs = require('fs')
+const fsExtra = require('fs-extra')
+
+const dir = './dist/git-dist';
+
+if (fs.existsSync(dir)) {
+  fs.rmdirSync(dir, { recursive: true });
+}
+fs.mkdirSync(dir)
 
 // Change Working directory
-process.chdir('dist/spa');
+process.chdir(dir);
 
-// execSync(`git remote add origin git@github.com:AaronHernandezPerez/covid-tracker-front-dist.git`, { stdio: 'inherit' })
+execSync(`git clone git@github.com:AaronHernandezPerez/covid-tracker-front-dist.git .`, { stdio: 'inherit' })
+execSync(`git rm -rf .`, { stdio: 'inherit' })
+fsExtra.copySync('../spa', '.')
 execSync(`git add -A`, { stdio: 'inherit' })
 
 const dateNow = new Date().toISOString().split('.').shift();
-execSync(`git commit -m "Build ${version} at ${dateNow}"`, { stdio: 'inherit' })
+try {
+  execSync(`git commit -m "Build ${version} at ${dateNow}"`, { stdio: 'inherit' })
+  execSync(`git push -f origin master`, { stdio: 'inherit' })
+} catch (error) {
+  // console.error(error);
+  console.log('No changes detected, nothing commited');
+}
 
-execSync(`git push -f git@github.com:AaronHernandezPerez/covid-tracker-front-dist.git master`, { stdio: 'inherit' })
+fs.rmdirSync(dir, { recursive: true });
